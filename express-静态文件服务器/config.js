6824d2d34ee193637846875fbdd0c330012c4895
unknown <1610826934@qq.com>
2019-06-27 13:59:47 +0800

    file

--- express-静态文件服务器/config.js
+++ express-静态文件服务器/config.js
@@ -2,9 +2,7 @@
 var fs = require('fs');
 var globalConfig = {}
 var conf = fs.readFileSync('./server.conf');
-
 var configArr = conf.toString().split('\n');
-
 for (var i = 0; i < configArr.length; i++) {
     globalConfig[configArr[i].split('=')[0]] = configArr[i].split('=')[1];
 }
--- express-静态文件服务器/index.js
+++ express-静态文件服务器/index.js
@@ -5,6 +5,6 @@ var globalConfig = require('./config');
 var app = new express();
 app.use(express.static(globalConfig['page_path']));
 
-var server = app.listen(globalConfig['port'],function () {
+var server = app.listen(555 ,function () {
     console.log('访问地址是http://localhost:555')
 })
